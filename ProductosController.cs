c54c8b71ac5e83ce83ad954731b9202d74e573a2
EduardoG742 <89094396+EduardoG742@users.noreply.github.com>
2021-10-12 18:32:04 -0500

    Add files via upload

---
+++ ProductosController.cs
@@ -0,0 +1,49 @@
+﻿using AppMVC01.Datos;
+using AppMVC01.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+    
+
+namespace AppMVC01.Controllers
+{
+    public class ProductosController : Controller
+    {
+        ProductosAdmin admin = new ProductosAdmin();
+        // GET: Productos
+        public ActionResult Index()
+        {
+            return View(admin.Consultar());
+        }
+        public ActionResult Detalle(int id)
+        {
+            return View(admin.Consultar(id));
+        }
+        public ActionResult Crear()
+        {
+            return View();
+        }
+        public ActionResult Guardar(PRODUCTOS modelo)
+        {
+            admin.Guardar(modelo);
+            return View("Crear", modelo);
+        }
+        public ActionResult Editar(int id)
+        {
+            return View(admin.Consultar(id));
+        }
+        public ActionResult Modificar(PRODUCTOS modelo)
+        {
+            admin.Modificar(modelo);
+            return View("Editar", modelo);
+        }
+        public ActionResult Eliminar(int id)
+        {
+            PRODUCTOS modelo = admin.Consultar(id);
+            admin.Eliminar(modelo);
+            return View("Index", admin.Consultar());
+        }
+    }
+}
---
+++ VentasController.cs
@@ -0,0 +1,48 @@
+﻿using AppMVC01.Datos;
+using AppMVC01.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppMVC01.Controllers
+{
+    public class VentasController : Controller
+    {
+        VentasAdmin admin = new VentasAdmin();
+        // GET: Ventas
+        public ActionResult Index()
+        {
+            return View(admin.Consultar());
+        }
+        public ActionResult Detalle(int id)
+        {
+            return View(admin.Consultar(id));
+        }
+        public ActionResult Crear()
+        {
+            return View();
+        }
+        public ActionResult Guardar(VENTAS modelo)
+        {
+            admin.Guardar(modelo);
+            return View("Crear", modelo);
+        }
+        public ActionResult Editar(int id)
+        {
+            return View(admin.Consultar(id));
+        }
+        public ActionResult Modificar(VENTAS modelo)
+        {
+            admin.Modificar(modelo);
+            return View("Editar", modelo);
+        }
+        public ActionResult Eliminar(int id)
+        {
+            VENTAS modelo = admin.Consultar(id);
+            admin.Eliminar(modelo);
+            return View("Index", admin.Consultar());
+        }
+    }
+}
